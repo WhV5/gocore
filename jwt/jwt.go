d01725d3706fddaf4c55c4f5bf57d4592e6ab7b0
pssauron <pssauron889@gmail.com>
2019-06-21 18:03:25 +0800

    重写Jwt 库

--- jwt/jwt.go
+++ jwt/jwt.go
@@ -1,142 +1,133 @@
 package jwt
 
 import (
-	"crypto/hmac"
 	"crypto/sha256"
 	"encoding/base64"
 	"encoding/json"
 	"errors"
-	"fmt"
-	"strings"
 	"time"
 )
 
 //Token jwtToken 结构体
-type Token struct {
-	Header    map[string]string
-	Payload   map[string]interface{}
-	Signature string
+type token struct {
+	header    header
+	payload   payload
+	signature string
 }
 
-//NewToken 返回新的token,这里为了方便 算法统一采用 HS256 算法
-//用户需要传入过期时间戳,token 验证时候验证是否过期
-func NewToken(secret string, expire int64, data interface{}) (*Token, error) {
-	header := map[string]string{
-		"alg": "HS256",
-		"typ": "JWT",
-	}
-
-	payload := map[string]interface{}{
-		"expire": expire,
-		"data":   data,
-	}
-
-	t := &Token{
-		Header:  header,
-		Payload: payload,
-	}
-
-	err := t.signToken(secret)
-
-	return t, err
-
+type header struct {
+	alg string //签名算法  hmac sha256
+	typ string //令牌类型  jwt
 }
 
-//ValidateToken  check token exists and expire
-func ValidateToken(secret, token string) (interface{}, error) {
-
-	return verify(token, secret)
-
+type payload struct {
+	iss  string      //签发人
+	exp  int64       //过期时间
+	sub  string      //主题
+	iat  time.Time   //签发时间
+	data interface{} //内容
 }
 
-//signToken token 验签
-func (t *Token) signToken(secret string) error {
-
-	hb, err := json.Marshal(t.Header)
-
-	if err != nil {
-
-		return errors.New("json marshal header errs")
+func newToken(secret string, expire int64, data interface{}) (*token, error) {
+	h := header{
+		alg: "hs256",
+		typ: "jwt",
 	}
 
-	pb, err := json.Marshal(t.Payload)
-
-	if err != nil {
-
-		return errors.New("json marshal payload err")
+	p := payload{
+		iss:  "",
+		exp:  expire,
+		sub:  "",
+		iat:  time.Now(),
+		data: data,
 	}
 
-	s := encode(hb) + "." + encode(pb)
-
-	sign := encrypt(secret, s)
-
-	t.Signature = sign
-
-	return nil
-}
-
-//base64 将字符串base64 成字符串
-func encode(b []byte) string {
-	return base64.StdEncoding.EncodeToString(b)
-}
-
-//decode 将token 解码
-func decode(pstr string) (map[string]interface{}, error) {
-	b, err := base64.StdEncoding.DecodeString(pstr)
-
-	if err != nil {
-
-		return nil, errors.New("token part decode err")
-	}
-	m := make(map[string]interface{})
-
-	err = json.Unmarshal(b, &m)
-
-	return m, nil
-}
-
-//encrypt hmac encrypt and encode to string
-func encrypt(secret, pstr string) string {
-	h := hmac.New(sha256.New, []byte(secret))
-	h.Write([]byte(pstr))
-	return encode(h.Sum(nil))
-
-}
-
-//verify Check if the token has been tampered,
-func verify(token, secret string) (interface{}, error) {
-	t := strings.Split(token, ".")
-
-	if len(t) != 3 {
-
-		return nil, fmt.Errorf("token split errs, required 3 parts,but find %d parts", len(t))
-	}
-
-	pstr := t[0] + "." + t[1]
-
-	sign := encrypt(secret, pstr)
-
-	if sign != t[2] {
-		return nil, fmt.Errorf("signature errs;token has been tampered")
-	}
-
-	pb, err := decode(t[1])
+	signature, err := sign(h, p)
 
 	if err != nil {
 		return nil, err
 	}
 
-	now := time.Now().Unix()
+	return &token{header: h, payload: p, signature: signature}, nil
+}
 
-	if f := isExpire(pb["expire"].(int64), now); !f {
-		return nil, errors.New("token has expire")
+//NewTokenStr
+func NewTokenStr(secret string, expire int64, data interface{}) (string, error) {
+
+	t, err := newToken(secret, expire, data)
+
+	if err != nil {
+		return "", errors.New("generator token has error occurred")
 	}
 
-	return pb["data"], nil
+	sh, err := encode(t.header)
+
+	if err != nil {
+		return "", err
+	}
+
+	sp, err := encode(t.header)
+
+	jwtToken := sh + "." + sp + "." + t.signature
+
+	return jwtToken, nil
+}
+
+//VerifyToken 严重token
+func VerifyToken(ts string) bool {
+
+	return false
+}
+
+func sign(h header, p payload, secret string) (string, error) {
+	he, err := encode(h)
+
+	if err != nil {
+		return "", err
+	}
+
+	pe, err := encode(p)
+
+	if err != nil {
+		return "", err
+	}
+
+	psign := he + "." + pe
+
+	hmac := sha256.New()
+
+	_, err = hmac.Write([]byte(psign))
+
+	if err != nil {
+		return "", errors.New("sign error has occurred")
+	}
+
+	return base64.StdEncoding.EncodeToString(hmac.Sum(nil)), nil
 
 }
 
-//isExpire 是否过期
-func isExpire(old, now int64) bool {
-	return old >= now
+func encode(value interface{}) (string, error) {
+
+	b, err := json.Marshal(value)
+
+	if err != nil {
+		return "", errors.New("marshal json has error occurred")
+	}
+
+	return base64.StdEncoding.EncodeToString(b), nil
+}
+
+func decode(s string, v interface{}) error {
+	b, err := base64.StdEncoding.DecodeString(s)
+	if err != nil {
+		return errors.New("decode string has error occurred")
+	}
+
+	err = json.Unmarshal(b, &v)
+
+	if err != nil {
+		return errors.New("unmarshal has error occurred")
+	}
+
+	return nil
 }
